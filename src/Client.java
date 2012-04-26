97acab2d2cd79548cd31e210763070532da36efd
Francisco Soares <fmssn@cin.ufpe.br>
2012-04-25 23:41:42 -0300

    Just a small change.

--- src/Client.java
+++ src/Client.java
@@ -14,7 +14,7 @@ public class Client {
 	
 	public static void main(String[] args) {
 		try {
-			Socket s = new Socket("127.0.0.1",9000);
+			Socket s = new Socket("127.0.1.1",9000);
 			
 			
 			s.close();
